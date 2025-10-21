cc00e7e6c080ad68a18fda50b3c38c29dee60dc2
lazaralexandra29 <llazar.aalexandra@gmail.com>
2025-10-21 20:34:18 +0300

    Implement basic GPIO initialization and define constants

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
+#include <avr/io.h>
+
 #include <drivers/gpio_driver.h>
+#include <drivers/timer_driver.h>
+#include <drivers/interrupts_driver.h>
+#include <drivers/usart_driver.h>
+#include <app/pedestrian_crossing.h>
+#include <app/traffic_light.h>
+#include <config/main_defines.h>
 
 GpioDriver myGpio;
 
-int main(){
-  
+int main(void){
+
+    while (1) 
+    {
+
+    }
 }
