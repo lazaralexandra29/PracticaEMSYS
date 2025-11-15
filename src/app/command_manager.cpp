009ab5a6d3fd869cf87ca0752d92ca458b9cace6
lazaralexandra29 <llazar.aalexandra@gmail.com>
2025-11-15 14:22:41 +0200

    feat(pin-config): define pins for traffic light system

--- src/app/command_manager.cpp
+++ src/app/command_manager.cpp
@@ -1,6 +1,7 @@
 #include "app/command_manager.hpp"
 #include "app/led_manager.hpp"
-#include "app/button_manager.hpp" 
+#include "app/button_manager.hpp"
+#include "app/pedestrian_button_manager.hpp"
 #include "drivers/usart_driver.hpp"
 #include <string.h>
 #include <stdlib.h>
@@ -11,6 +12,13 @@ void CommandManager::executeCommand(const char* command)
     char buffer[64];
     strncpy(buffer, command, sizeof(buffer) - 1);
     buffer[sizeof(buffer) - 1] = '\0';
+    
+    size_t len = strlen(buffer);
+    while (len > 0 && (buffer[len-1] == ' ' || buffer[len-1] == '\t' || buffer[len-1] == '\r' || buffer[len-1] == '\n'))
+    {
+        buffer[len-1] = '\0';
+        len--;
+    }
 
     char* token = strtok(buffer, " ");
     if (token == NULL) 
@@ -19,53 +27,28 @@ void CommandManager::executeCommand(const char* command)
         return;
     }
 
-    if (strcmp(token, "SetLED") == 0)
+    if (strcmp(token, "night") == 0 || strcmp(token, "Night") == 0 || strcmp(token, "NIGHT") == 0)
     {
-        char* indexStr = strtok(NULL, " "); 
-        char* stateStr = strtok(NULL, " ");
-        
-        if (indexStr && stateStr)
-        {
-            uint8_t index = atoi(indexStr);
-            LedManager::handle(index, stateStr);
-        }
-        else
-        {
-            UsartDriver::send("ERR: Usage -> SetLED <index> <ON|OFF|TOGGLE>\r\n");
-        }
+        PedestrianButtonManager::handleNight();
     }
 
-    else if (strcmp(token, "GetLEDState") == 0)
+    else if (strcmp(token, "day") == 0 || strcmp(token, "Day") == 0 || strcmp(token, "DAY") == 0)
     {
-        char* indexStr = strtok(NULL, " ");
-        
-        if (indexStr)
-        {
-            uint8_t index = atoi(indexStr);
-            LedManager::reportLedState(index); 
-        }
-        else
-        {
-            UsartDriver::send("ERR: Usage -> GetLEDState <index>\r\n");
-        }
+        PedestrianButtonManager::handleDay();
     }
 
-    else if (strcmp(token, "ButtonInfo") == 0)
-    {
-        ButtonManager::reportState(); 
-    }
-
-    else if (strcmp(token, "Help") == 0)
+    else if (strcmp(token, "Help") == 0 || strcmp(token, "help") == 0 || strcmp(token, "HELP") == 0)
     {
         UsartDriver::send("\r\n--- Available Commands ---\r\n");
-        UsartDriver::send("SetLED <idx> <ON|OFF|TOGGLE>\r\n");
-        UsartDriver::send("GetLEDState <idx>\r\n");
-        UsartDriver::send("ButtonInfo\r\n");
+        UsartDriver::send("night - Activate night mode\r\n");
+        UsartDriver::send("day - Deactivate night mode\r\n");
+        UsartDriver::send("help - Show this help\r\n");
+        UsartDriver::send("exit - Exit program\r\n");
         UsartDriver::send("--------------------------\r\n");
     }
     
     else
     {
-        UsartDriver::send("ERR: Unknown command. Type 'Help'.\r\n");
+        UsartDriver::send("ERR: Unknown command. Type 'help'.\r\n");
     }
 }
